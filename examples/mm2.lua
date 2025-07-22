c1ae90cbe7bf644786e6bcc1f351225e7599ee20
tul <heytul.ontop@gmail.com>
2025-07-23 02:47:40 +0300

    Add files via upload

---
+++ examples/mm2.lua
@@ -0,0 +1,10 @@
+-- // esp-lib.lua
+local esplib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tulontop/esp-lib.lua/refs/heads/main/source.lua"))()
+
+-- // mm2 coin esp
+game.Workspace.DescendantAdded:Connect(function(coin)
+    if coin.Name == "Coin_Server" then
+        esplib.add_box(coin)
+        esplib.add_name(coin)
+    end
+end)
